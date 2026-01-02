b96b000cbe73b6cd365b81a62ffd9703b0c02858
devzone2026 <devzone2026@gmail.com>
2026-01-02 21:37:57 +0530

    logs

--- middleware/firebaseAuth.js
+++ middleware/firebaseAuth.js
@@ -2,11 +2,14 @@ import admin from "../config/firebase.js";
 
 export default async function firebaseAuth(req, res, next) {
   const token = req.headers.authorization?.replace("Bearer ", "");
-
+  console.log("token>>>> ", token);
+  
   if (!token) return res.status(401).json({ message: "No token provided" });
 
   try {
     const decoded = await admin.auth().verifyIdToken(token);
+    console.log("decoded >>> ", decoded);
+    
     req.user = decoded;
     next();
   } catch (err) {
